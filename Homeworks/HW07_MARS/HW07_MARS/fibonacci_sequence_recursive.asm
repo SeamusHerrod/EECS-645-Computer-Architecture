079edcb3e5ff58f6cd366ca8fd216fc14384f8f4
Seamus Herrod <85411251+SeamusHerrod@users.noreply.github.com>
2023-04-06 17:38:15 -0600

    Finished!

--- Homeworks/HW07_MARS/HW07_MARS/fibonacci_sequence_recursive.asm
+++ Homeworks/HW07_MARS/HW07_MARS/fibonacci_sequence_recursive.asm
@@ -1,5 +1,5 @@
 
-caller:		addi 	$a0, $zero, 3		# initialize argument register $a0 with n = 18
+caller:		addi 	$a0, $zero, 18		# initialize argument register $a0 with n = 18
 #######		#addi 	$a1, $zero, 0x100100A0	# this instruction is treated as a pseudo instruction, replace it with the proper basic MIPS instructions
 		lui	$a1, 4097
 		ori	$a1, 160
@@ -14,16 +14,23 @@ next:		j	exit
 fib_seq:
 ####################### Insert Your Code Here ################################
 	
+	addi $a0, $a0, 1
+	addi $sp, $sp, -8
+	sw   $ra, 4($sp)
+	sw   $a0, 0($sp)
 	add  $t0, $0, $a0
 	add  $a0, $0, $0
 LOOP:	beq  $a0, $t0, END
 	jal FIB
+	lw   $t0, 0($sp)
 	sw   $v0, 0($a1)
 	addi $a1, $a1, 4
 	addi $a0, $a0, 1
 	j LOOP
 
-END:	jr $ra
+END:	lw   $ra, 4($sp)
+	addi $sp, $sp, 8
+	jr $ra
 
 
 FIB:    addi $sp $sp, -12
